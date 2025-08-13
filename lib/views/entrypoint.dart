1fd3e554a5ecead5c007c184f54232446dcaaafd
Zekariyas <99519232+ZekariyasT@users.noreply.github.com>
2025-08-13 23:40:39 +0300

    add list of widgets

--- lib/views/entrypoint.dart
+++ lib/views/entrypoint.dart
@@ -1,12 +1,23 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:foodly/constants/constants.dart';
 import 'package:foodly/controllers/tab_index_controller.dart';
+import 'package:foodly/views/cart/cart_page.dart';
+import 'package:foodly/views/home/home_page.dart';
+import 'package:foodly/views/profile/profile_page.dart';
+import 'package:foodly/views/search/search_page.dart';
 import 'package:get/get.dart';
 
 class MainScreen extends StatelessWidget {
   MainScreen({super.key});
-  List<Widget> pageList = [];
+  List<Widget> pageList = const [
+    HomePage(),
+    SearchPage(),
+    CartPage(),
+    ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +26,7 @@ class MainScreen extends StatelessWidget {
       () => Scaffold(
         body: Stack(
           children: [
-            Container(height: hieght, width: width, color: kOffWhite),
+            pageList[controller.tabIndex],
             Align(
               alignment: Alignment.bottomCenter,
               child: Theme(
